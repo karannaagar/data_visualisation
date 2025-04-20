710cb934319808d5cb20636db75c4e6ef5d2a119
karan nagar <karan.bmx@gmail.com>
2025-04-20 15:41:06 +0530

    removed x n y axis

--- rw_visual.py
+++ rw_visual.py
@@ -13,10 +13,18 @@ while True:
 
     fig, ax = plt.subplots()
 
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+
     plt.show()
+
+
 
     user_input = input("do you want to continue? press y/n : ")
     if user_input == 'n':
